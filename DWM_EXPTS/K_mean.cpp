aac5adcd0ec097409413fc6747f8fe54caa885cc
DevanshMahoday <113814933+DeV-21@users.noreply.github.com>
2023-11-03 00:05:47 +0530

    Update K_mean.cpp

--- DWM_EXPTS/K_mean.cpp
+++ DWM_EXPTS/K_mean.cpp
@@ -119,3 +119,48 @@ Enter Number 7: 30
 Enter Number 8: 11
 Enter Number 9: 25
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+K-Means Clustering is an Unsupervised Learning algorithm, which groups the unlabeled dataset
+into different clusters. Here K defines the number of pre-defined clusters that need to be created
+in the process, as if K=2, there will be two clusters, and for K=3, there will be three clusters, and
+so on.
+The k-means clustering algorithm mainly performs two tasks:
+o Determines the best value for K center points or centroids by an iterative process.
+
+o Assigns each data point to its closest k-center. Those data points which are near to the particular k-
+center, create a cluster.
+
+Hence each cluster has datapoints with some commonalities, and it is away from other clusters.
+The working of the K-Means algorithm is explained in the below steps:
+Step-1: Select the number K to decide the number of clusters.
+Step-2: Select random K points or centroids. (It can be other from the input dataset).
+Step-3: Assign each data point to their closest centroid, which will form the predefined K clusters.
+Step-4: Calculate the variance and place a new centroid of each cluster.
+Step-5: Repeat the third steps, which means reassign each datapoint to the new closest centroid of
+each cluster.
+Step-6: If any reassignment occurs, then go to step-4 else go to FINISH.
+Step-7: The model is ready.
+*/
